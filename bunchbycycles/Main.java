e6421a743b9eb5dbb079a4754d7e1fcb4d13114e
AdithyaSAcharya <103981751+AdithyaSAcharya@users.noreply.github.com>
2022-09-22 18:00:28 +0530

    Update Main.java

--- bunchbycycles/Main.java
+++ bunchbycycles/Main.java
@@ -1,4 +1,4 @@
-package bunchbycycles;
+
 
 public class Main {
   static class CountsByUsage {
@@ -8,7 +8,20 @@ public class Main {
   };
 
   static CountsByUsage countBatteriesByUsage(int[] cycles) {
-    CountsByUsage counts = new CountsByUsage();
+    CountsByUsage counts = new CountsByUsage();//this  is object of class CountByUsage
+    //And we have lowcount medium count and high count as its element and will run through all the constraint given here and return 
+    for(int i=0;i<cycles.length;i++){
+        if(cycles[i]<410){
+            counts.lowCount=counts.lowCount+1;
+        }
+        else if(cycles[i]>410 && cycles[i]<909){
+            counts.mediumCount=counts.mediumCount+1;
+        }
+        else if(cycles[i]>910){
+            counts.highCount=counts.highCount+1;
+            
+        }
+    }
     return counts;
   }
 
@@ -18,6 +31,12 @@ public class Main {
     assert(counts.lowCount == 2);
     assert(counts.mediumCount == 3);
     assert(counts.highCount == 1);
+    
+    
+    //here it will print the values for the low count ,mid count ,high Count.
+    System.out.println("Low Count is :"+counts.lowCount);
+    System.out.println("Medium Count is :"+counts.mediumCount);
+    System.out.println("High Count is :"+counts.highCount);
     System.out.println("Done counting :)\n");
   }
 
